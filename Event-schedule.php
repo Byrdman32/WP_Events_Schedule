1ff7e9ff9c55213f9f3b37592a7b6a42031c3a3b
Eli Byrd <edbgkk@mst.edu>
2022-10-06 20:53:09 -0500

    Initial Commit

---
+++ Event-schedule.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Event Schedule
+Plugin URI: https://github.com/Byrdman32/WP_Events_Schedule
+Description: Custom WordPress Plugin that generates a schedule for your event.
+Version: 0.1
+Author: Eli Byrd
+Author URI: https://github.com/Byrdman32
+License: GPL2 or later
+*/
